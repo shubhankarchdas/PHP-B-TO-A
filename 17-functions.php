1eedd0af2df37bde8ed7daf45598917a3fa6da6b
shubhankarchdas <shubhankarchdas@gmail.com>
2025-08-20 21:28:08 +0530

    all about strings and functions

---
+++ 17-functions.php
@@ -0,0 +1,36 @@
+<?php
+
+function myMessage() {
+  echo "Hello world!";
+}
+
+myMessage();
+
+echo "<br>";
+
+
+// PHP Function Arguments
+
+function familyName($fname) {
+    echo "$fname Hiii.<br>";
+}
+
+familyName("Jani");
+familyName("Hege");
+familyName("Stale");
+familyName("Kai Jim");
+familyName("Borge");
+
+
+echo "<br>";
+
+function sum($x, $y) {
+  $z = $x + $y;
+  return $z;
+}
+
+echo "5 + 10 = " . sum(5, 10) . "<br>";
+echo "7 + 13 = " . sum(7, 13) . "<br>";
+echo "2 + 4 = " . sum(2, 4);
+
+?>
